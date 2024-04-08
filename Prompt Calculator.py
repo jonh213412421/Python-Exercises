966ec10e0cfa760d817d8b3421a2dc8f8822c6ea
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-04-08 20:48:33 -0300

    Update Prompt Calculator.py

--- Prompt Calculator.py
+++ Prompt Calculator.py
@@ -3,26 +3,28 @@ import sys
 
 class calc:
     def eval_func(self):
-        func = sys.argv[2] #receives args
-        result = eval(func) #eval string
-        return result
+        if len(sys.argv) < 3: #checks args
+            print("Insuficient args")
+        else:
+            func = sys.argv[2] #gets func as string
+            result = eval(func) #evals string
+            print(result)
+            return result
     def eval_func_file(self):
-        self.file = sys.argv[2]
-        with open(self.file, "r") as f:
-            func = f.read() #read args from file
-            f.close()
-        result = eval(func) #eval string
-        return result
+        if len(sys.argv) < 3:
+            print("Insuficient args")
+        else:
+            self.file = sys.argv[2] #gets path to file
+            with open(self.file, "r") as f:
+                func = f.read() #read string expression to func
+                f.close()
+            result = eval(func) #eval func
+            print(result)
+            return result
 
 if __name__ == "__main__":
     method = sys.argv[1]
     if method == "eval_file":
-        if len(sys.argv) < 2:
-            print("insuficient args")
-        result = calc().eval_func_file() #eval file
-        print(result)
+        result = calc().eval_func_file()
     if method == "eval_func":
-        if len(sys.argv) < 2:
-            print("insuficient args")
-        result = calc().eval_func() #eval func
-        print(result)
+        result = calc().eval_func()
