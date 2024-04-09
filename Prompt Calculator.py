87dac55979cdc98c8e898c62a457e7a09d974fe6
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-04-09 19:44:10 -0300

    Update Prompt Calculator.py

--- Prompt Calculator.py
+++ Prompt Calculator.py
@@ -2,6 +2,24 @@
 import sys
 
 class calc:
+        def eval_matrix(self):
+        x = []
+        vars = []
+        if len(sys.argv) < 3:
+            print("Insuficient args")
+        else:
+            self.file = sys.argv[2]
+            with open(self.file, "r") as f:
+                lines = f.readlines()
+                f.close()
+            for line in lines:
+                line = line.split()
+                x.append(line)
+            self.data = np.array(x, dtype=float)
+            pl, solved_matrix = lu(self.data, permute_l=True)
+            print(solved_matrix)
+            return solved_matrix
+            
     def eval_func(self):
         if len(sys.argv) < 3: #checks args
             print("Insuficient args")
@@ -10,6 +28,7 @@ class calc:
             result = eval(func) #evals string
             print(result)
             return result
+            
     def eval_func_file(self):
         if len(sys.argv) < 3:
             print("Insuficient args")
@@ -28,3 +47,5 @@ if __name__ == "__main__":
         result = calc().eval_func_file()
     if method == "eval_func":
         result = calc().eval_func()
+    if method == "eval_matrix":
+        result = calc().eval_matrix()
