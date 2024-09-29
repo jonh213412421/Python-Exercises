16bc056fd124bdba362c483bcf4a4bf7f0b67bf4
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-09-29 12:12:18 -0300

    Update Fill Field in Form in a Site.py

--- Fill Field in Form in a Site.py
+++ Fill Field in Form in a Site.py
@@ -1,15 +1,20 @@
-import time
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# Configuração do WebDriver
-driver = webdriver.Chrome()
+opcoes = Options()
+opcoes.add_argument("--headless=old")
+opcoes.add_argument("--disable-gpu")
+#não mostrar blank page
+opcoes.add_argument("--window-position=-2400,-2400")
 
-# Abrir o site com o formulário. Neste exemplo, o site da receita
+driver = webdriver.Chrome(options=opcoes)
+
+# Abrir o site com o formulário
 driver.get("https://servicos.receita.fazenda.gov.br/Servicos/CPF/InscricaoPublica/inscricao.asp")
+
 
 # Espera carregar o elemento
 elemento = WebDriverWait(driver, 10).until(
@@ -19,8 +24,7 @@ elemento = WebDriverWait(driver, 10).until(
 # Selecionar o campo "dia" e escolher um valor
 day_select = driver.find_element(By.ID, 'txtNascimento')
 day_select.send_keys('25/12/2025')  # Seleciona o dia 25/12/2025
-
-time.sleep(10)
+print(driver.page_source)
 
 # Fechar o driver após completar as ações
 driver.quit()
