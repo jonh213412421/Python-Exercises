1457db526ebc00be024bfc119746875666b214ad
João Alves <138532454+jonh213412421@users.noreply.github.com>
2025-03-09 19:44:26 -0300

    Update main.py

--- Cryptography/main.py
+++ Cryptography/main.py
@@ -11,16 +11,33 @@ if not os.path.exists(os.path.join(os.getcwd(), "cifra")):
 if not os.path.exists(os.path.join(os.getcwd(), "mensagens")):
     os.mkdir(os.path.join(os.getcwd(), "mensagens"))
 
-print(len(sys.argv))
 if len(sys.argv) == 0:
     print("funções:\n "
           "encrypt (mensagem)/(caso não tenha mensagem, ele abrirá um dialog para carregar um documento de texto) -> criptografa mensagem\n"
           "decrypt (mensagem)/(caso não tenha mensagem, ele abrirá um dialog para carregar um arquivo criptografado) -> descriptografa mensagem\n")
 if sys.argv[1] == "encrypt" and len(sys.argv) == 2:
-    caminho_arquivo = filedialog.askopenfile()
+    caminho_arquivo = filedialog.askopenfile(title="escolha o caminho do arquivo")
     with open(caminho_arquivo, "r") as f:
         texto = f.read()
+        f.close()
     encrypt.encrypt(texto)
 if sys.argv[1] == "encrypt" and len(sys.argv) > 2:
     texto = ''.join(sys.argv[2:])
     encrypt.encrypt(texto)
+    
+if sys.argv[1] == "decrypt_a" and len(sys.argv) == 2:
+    caminho_chave = filedialog.askopenfile(title="escolha o caminho da chave")
+    caminho_cifra = filedialog.askopenfile(title="escolha o caminho da cifra")
+    with open(caminho_chave, "r") as f:
+        chave = f.read()
+        f.close()
+    with open(caminho_cifra, "r") as f:
+        cifra = f.read()
+        f.close()
+    decrypt.decrypt(chave, cifra)
+if sys.argv[1] == "decrypt_s" and len(sys.argv) == 2:
+    chave = sys.argv[2]
+    texto = sys.argv[3]
+    encrypt.encrypt(texto)
+if sys.argv[1] == "decrypt_s" and len(sys.argv) > 2:
+    print("argumentos incorretos")
