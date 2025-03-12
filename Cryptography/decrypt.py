29918be4c6dfdad24f6836be251429cd5cb7274b
João Alves <138532454+jonh213412421@users.noreply.github.com>
2025-03-11 21:49:41 -0300

    Update decrypt.py

--- Cryptography/decrypt.py
+++ Cryptography/decrypt.py
@@ -4,13 +4,13 @@ def decrypt(caminho_cifra: str, caminho_chave: str) -> None:
 
     mensagem = ""
     dir_mensagem = os.path.join(os.getcwd(), "mensagens")
-    with open(caminho_chave, "r") as f:
+    with open(caminho_chave, "rb") as f:
         chave2 = f.read()
         f.close()
-    with open(caminho_cifra, "r") as f:
+    with open(caminho_cifra, "rb") as f:
         cifra = f.read()
         f.close()
-    xor = ''.join(str(int(chave2[i]) ^ int(cifra[i])) for i in range(len(chave2)))
+    xor = ''.join('1' if chave2[i] != cifra[i] else '0' for i in range(len(chave2)))
     for i in range(0, len(xor), 8):
         byte = int(xor[i:i + 8], 2)
         char = chr(byte)
@@ -31,3 +31,4 @@ def decrypt(caminho_cifra: str, caminho_chave: str) -> None:
             print("Processo concluído com êxito!")
         except:
             print("Erro!")
+            
